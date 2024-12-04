9821b74890d1dee26d1c336f07c1944755150ad8
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2024-12-04 21:31:20 +0000

    armstrong without using string

--- code/armstring2.java
+++ code/armstring2.java
@@ -1,5 +1,23 @@
 // other than string
+import java.util.*;
 
 public class armstring2 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = n;
+        int sum = 0;
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (m>0) {
+            arr.add(m%10);
+            m = m/10;
+        }
+        for (Integer integer : arr) {
+            sum=sum+(int)(Math.pow(integer,arr.size()));
+        }
+        if(sum==n){
+            System.out.println("armstrong");
+        }
+        sc.close();
+    }
 }
