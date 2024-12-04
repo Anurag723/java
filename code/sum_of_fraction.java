feda4f04b8e9b97d5df2196808fc35db6fba10ac
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2024-12-04 21:57:40 +0000

    doen

---
+++ code/sum_of_fraction.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class sum_of_fraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    }
+}
