3daba99a2dad9f1cd47987b51f69a3451bab7c6c
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2024-12-05 19:58:59 +0000

    done

--- code/sum_of_fraction.java
+++ code/sum_of_fraction.java
@@ -3,6 +3,11 @@ public class sum_of_fraction {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        double sum = 0;
+        for(int i=1;i<=n;i++){
+            sum = sum+(1.0/i);
+        }
+        System.out.println(sum);
         sc.close();
     }
 }
