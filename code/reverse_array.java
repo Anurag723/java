878991e6ff0620cac8d9f8d887d343632125502a
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-08 16:44:06 +0000

    rev done

---
+++ code/reverse_array.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class reverse_array{
+    public static int[] rev(int[] arr){
+        int temp = 0;
+        for(int i=0;i<arr.length/2;i++){
+            temp = arr[i];
+            arr[i] = arr[arr.length-1-i];
+            arr[arr.length-1-i] = temp;
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        rev(arr);
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
