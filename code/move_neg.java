1c8a1838a1910e6516cbd4337b840e19ce24ad61
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-09 18:26:47 +0000

    negative at beginning

---
+++ code/move_neg.java
@@ -0,0 +1,27 @@
+public class move_neg {
+    public static void srt(int[] arr){
+        int n = arr.length;
+
+        int j = 0;
+        for(int i=0;i<n;i++){
+            if (arr[i]<0) {
+                swap(arr,i,j);
+                j++;
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int lo, int hi){
+        int temp = arr[lo];
+        arr[lo] = arr[hi];
+        arr[hi] = temp;
+    }
+
+    public static void main(String[] main){
+        int[] arr = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
+        srt(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
