eecfc0c9ac5bc819fbdb70934c8aa2edd3fe3eb7
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-12 18:09:36 +0000

    using two pointer approach

--- code/twosum.java
+++ code/twosum.java
@@ -1,6 +1,10 @@
 import java.util.*;
 
 public class twosum{
+
+    // tc = O(NlogN)
+
+
     static int[] ts(int[] arr, int target){
         
         int[] res = new int[2];
@@ -16,11 +20,38 @@ public class twosum{
         }
         return res;
     }
+
+
+    // using greedy approach with pointer for left and right
+
+    static int[] ts1(int[] arr, int target){
+        int[] res = new int[2];
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left<right) {
+            if (arr[left]+arr[right]==target) {
+                res[0] = arr[left];
+                res[1] = arr[right];
+                return res;
+            }
+
+            else if(arr[left]+arr[right]<target){
+                left++;
+            }
+            else if (arr[left]+arr[right]>target) {
+                right--;
+            }
+        }
+
+        return res;
+    }
     public static void main(String[] args){
         int[] arr = {2,6,5,8,11};
         int target = 14;
+        Arrays.sort(arr);
 
-        int[] res = ts(arr, target);
+        int[] res = ts1(arr, target);
 
         for (int i : res) {
             System.out.print(i+" ");
