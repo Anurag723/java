115b39e788607dff0d168ebb5eba1d2064e16c58
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-12 18:33:54 +0000

    multiple

--- code/buy_sell_stock.java
+++ code/buy_sell_stock.java
@@ -15,8 +15,19 @@ public class buy_sell_stock {
         return max;
     }
 
+    // for multiple
+    static int multiple(int[] arr){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i>0 && arr[i-1]<arr[i]) {
+                sum+=arr[i]-arr[i-1];
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {7,1,5,3,6,4};
-        System.out.println(single(arr));
+        int[] arr = {100, 180, 260, 310, 40, 535, 695};
+        System.out.println(multiple(arr));
     }
 }
