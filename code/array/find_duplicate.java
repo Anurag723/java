11023293b2e6b592a82be9d2ee0daf397621c337
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-14 18:16:19 +0000

    duplicate element in array

---
+++ code/array/find_duplicate.java
@@ -0,0 +1,20 @@
+
+
+public class find_duplicate {
+    public static int rmv(int[] arr) {
+        // Code Here
+        if(arr.length<=1)
+            return arr.length;
+        int idx = 1;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arr[i-1])
+                arr[idx++] = arr[i];
+        }
+        
+        return idx;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,2,5,3,7,8,9};
+        System.out.println(rmv(arr));
+    }
+}
