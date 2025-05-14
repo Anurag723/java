bebffc90da7f86ffbaf153e483f23e737febd692
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-14 18:34:35 +0000

    bug removed

--- code/array/find_duplicate.java
+++ code/array/find_duplicate.java
@@ -1,17 +1,24 @@
-
+import java.util.*;
 
 public class find_duplicate {
     public static int rmv(int[] arr) {
         // Code Here have bugs
-        if(arr.length<=1)
-            return arr.length;
-            
-        int idx = 1;
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]!=arr[i-1])
-                arr[idx++] = arr[i];
-        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int idx = 0;
         
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> set : map.entrySet()) {
+            int key = set.getKey();
+            int val = set.getValue();
+
+            if (val >= 2) {
+                idx = key;
+            }
+        }
+
         return idx;
     }
     public static void main(String[] args) {
