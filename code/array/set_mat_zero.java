fa35f8f5708e1f219dc05aa060268d286b8d3a77
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-15 18:29:45 +0000

    done both ways

--- code/array/set_mat_zero.java
+++ code/array/set_mat_zero.java
@@ -1,5 +1,45 @@
 public class set_mat_zero {
 
+    // Brute
+
+    static void setMatrixZeroes1(int[][] mat) {
+        int n = mat.length, m = mat[0].length;
+
+        // To store which rows and columns are
+        // supposed to mark with zeroes
+        boolean[] rows = new boolean[n];
+        boolean[] cols = new boolean[m];
+
+        // Traverse the matrix to fill rows[] and cols[]
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                // If the cell contains zero then mark
+                // its row and column as zero
+                if (mat[i][j] == 0) {
+                    rows[i] = true;
+                    cols[j] = true;
+                }
+            }
+        }
+
+        // Set matrix elements to zero if they
+        // belong to a marked row or column
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                // Mark cell (i, j) with zero if either
+                // of rows[i] or cols[j] is true
+                if (rows[i] || cols[j]) {
+                    mat[i][j] = 0;
+                }
+            }
+        }
+    }
+
+
+    // Optimised
+
     static void setMatrixZeroes(int[][] mat) {
         int n = mat.length, m = mat[0].length;
         int c0 = 1;
@@ -48,7 +88,7 @@ public class set_mat_zero {
     public static void main(String[] args) {
         int[][] arr = {{0,1,2,0},{3,4,0,2},{1,3,1,5}};
 
-        setMatrixZeroes(arr);
+        setMatrixZeroes1(arr);
 
         for(int i = 0;i<arr.length;i++){
             for (int j = 0; j < arr[i].length; j++) {
