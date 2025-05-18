88b1c2a88f044e95f8961ce51e7f714f08ea3574
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-18 18:02:51 +0000

    nextpermutation

---
+++ code/array/next_permutation.java
@@ -0,0 +1,13 @@
+package array;
+
+public class nextsermutation {
+
+
+    public static void main(String[] args){
+        int[] arr = {2,4,1,7,5,0};
+        np(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
---
+++ code/array/nextstermutation.java
@@ -0,0 +1,50 @@
+package array;
+
+public class nextstermutation {
+
+    static void np(int[] arr){
+        int pivot = -1;
+        int n = arr.length;
+
+        for(int i = n - 2; i >= 0; i--){
+            if (arr[i] < arr[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+    }
+
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public static void main(String[] args){
+        int[] arr = {2,4,1,7,5,0};
+        np(arr);
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
