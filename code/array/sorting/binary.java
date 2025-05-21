a14c1bdad434fbb3c88ec334c9e97aa55238bfe8
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-21 19:31:01 +0000

    binary searching algo

--- code/array/sorting/binary.java
+++ code/array/sorting/binary.java
@@ -8,24 +8,20 @@ public class binary {
             while (low <= high) {
                 int mid = low + (high - low) / 2;
 
-                // Check if x is present at mid
                 if (arr[mid] == x)
                     return mid;
 
-                // If x greater, ignore left half
                 if (arr[mid] < x)
                     low = mid + 1;
 
-                // If x is smaller, ignore right half
                 else
                     high = mid - 1;
             }
 
-            // If we reach here, then element was
-            // not present
             return -1;
         }
     }
+    
     public static void main(String[] args) {
         int[] arr = {2,5,7,8,4,9,6,3};
         int res = search(arr,7);
