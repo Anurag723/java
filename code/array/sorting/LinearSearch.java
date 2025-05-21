d70e2347e2b3a72ef6d21cbd85f5293d2a400919
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-21 19:23:30 +0000

    linear search

---
+++ code/array/sorting/LinearSearch.java
@@ -0,0 +1,23 @@
+package code.array.sorting;
+
+public class LinearSearch {
+
+    static int search(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,8,4,9,6,3};
+        int res = search(arr,7);
+        if (res>=0) {
+            System.out.println("found at "+res);
+        }
+        else
+            System.out.println("Not Found");
+    }
+}
