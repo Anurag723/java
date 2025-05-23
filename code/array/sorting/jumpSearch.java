c1afe4cac16ebf772911b828e07f1ef8ade80b67
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-05-23 18:26:08 +0000

    my soln

--- code/array/sorting/jumpSearch.java
+++ code/array/sorting/jumpSearch.java
@@ -1,6 +1,35 @@
 import java.util.*;
 
 class jumpSearch{
+    static int js(int[] arr, int target){
+        int n = arr.length;
+        int start = 0;
+        int step = (int)Math.sqrt(n);
+
+        while (start<n && arr[Math.min(n, step)-1]<target) {
+            start = step;
+            step += Math.sqrt(n);
+
+            if (step>n) {
+                return -1;
+            }
+        }
+
+        for(int i = start; i<Math.min(step,n); i++){
+            if (arr[i]==target) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
+
+
+
+
+
     static int search(int[] arr, int target) {
         int n = arr.length;
         int step = (int)Math.sqrt(n);
@@ -27,8 +56,10 @@ class jumpSearch{
         int[] arr = {5,4,7,8,9,1,2,3,6};
         Arrays.sort(arr);
         int res = search(arr,8);
+        int res1 = search(arr,8);
         if (res>=0) {
             System.out.println("found at "+res);
+            System.out.println("res1 is:-"+res1);
         }
         else
             System.out.println("Not Found");
