e9a9e0c2cb6115e356f6df0efe250a604fd0590d
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-06-01 18:33:54 +0000

    creation of a linked list in java

---
+++ code/linkedlist/ll1.java
@@ -0,0 +1,19 @@
+// code to create linked list part1(creation)
+
+package code.linkedlist;
+
+public class ll1 {
+
+    private ListNode head;
+
+    private static class ListNode {
+    
+        private int data;       //data to be kept in ll
+        private ListNode next;  //address of next node
+
+        public ListNode(int data){
+            this.data = data;       //data given with linked list
+            this.next = null;
+        }
+    }
+}
