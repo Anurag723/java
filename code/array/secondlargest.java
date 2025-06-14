24770e2a061cb4bf3cc4ce412dda78e761012682
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-06-14 18:33:38 +0000

    sec largest in array

---
+++ code/array/secondlargest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class secondlargest{
+    public static void main(String[] args) {
+        int[] arr = {2,5,6,9,8};
+
+        Arrays.sort(arr);
+
+        System.out.println("arr[arr.length-2]");
+    }
+
+}
