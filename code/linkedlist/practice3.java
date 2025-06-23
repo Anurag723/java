399cf9be7cb7ccdfcb2f7e390a568e86af0f042e
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-06-23 20:20:56 +0000

    revision circular linked list

--- code/linkedlist/practice3.java
+++ code/linkedlist/practice3.java
@@ -233,3 +233,104 @@ class doubll{
         System.out.println("null");
     }
 }
+
+class cirll{
+    private node last;
+    private int length;
+
+    private static class node {
+    
+        private int data;
+        private node next;
+
+        public node(int val){
+            this.data = val;
+        }
+    }
+
+    public cirll(){
+        this.length = 0;
+        this.last = null;
+    }
+
+    public boolean isEmpty(){
+        return length==0;
+    }
+
+    public void createcll(){
+        node first = new node(12);
+        length++;
+        node sec = new node(14);
+        length++;
+        node thi = new node(16);
+        length++;
+        node fou = new node(18);
+        length++;
+        node fiv = new node(20);
+        length++;
+
+        first.next = sec;
+        sec.next = thi;
+        thi.next = fou;
+        fou.next = fiv;
+        fiv.next = first;
+
+        last = fiv;
+    }
+
+    public void insbg(int data){
+        node nn = new node(data);
+
+        if (last==null) {
+            last = nn;
+            last.next = last;
+        }
+
+        else{
+            nn.next = last.next;
+        }
+        last.next = nn;
+        length++;
+    }
+
+    public void insend(int val){
+        node nn = new node(val);
+
+        if(last == null){
+            last = nn;
+            last.next = last;
+        }
+
+        else{
+            nn.next = last.next;
+            last.next = nn;
+            last = nn;
+        }
+        length++;
+    }
+
+    public node rmvfst(){
+        node nn = last.next;
+
+        if (last == last.next) {
+            last = null;
+        }
+
+        else{
+            last.next = nn.next;
+        }
+        nn.next = null;
+        length--;
+        return nn;
+    }
+
+    public void print(){
+        node nn = last.next;
+
+        while (nn != last) {
+            System.out.print(nn.data+"-");
+            nn = nn.next;
+        }
+        System.out.println(nn.data);
+    }
+}
