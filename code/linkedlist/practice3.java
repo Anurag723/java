7bb62a38b1edba5971962994d7418e419001c76a
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-06-23 19:14:26 +0000

    linked list revision

---
+++ code/linkedlist/practice3.java
@@ -0,0 +1,126 @@
+import java.util.*;
+public class practice3 {
+    private node head;
+
+    private static class node{
+        private int data;
+        private node next;
+
+        public node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void printll(){
+        node nn = head;
+
+        while (nn != null) {
+            System.out.print(nn.data+"->");
+            nn = nn.next;
+        }
+        System.out.println("null");
+    }
+
+    public void insbg(int data){
+        node nn = new node(data);
+        nn.next = head;
+        head = nn;
+    }
+
+    public void insed(int data){
+        node nn = new node(data);
+        if (head==null) {
+            head = nn;
+            return;
+        }
+
+        node curr = head;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        curr.next = nn;
+    }
+
+    public void insat(int val, int pos){
+        node nn = new node(val);
+        if (pos==1) {
+            nn.next = head;
+            head = nn;
+        }
+
+        node pre = head;
+        int count = 1;
+
+        while (count<pos-1) {
+            pre = pre.next;
+            count++;
+        }
+
+        node curr = pre.next;
+        nn.next = curr;
+        pre.next = nn;
+    }
+
+    public node deletefst(){
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        node temp = head;
+        head = head.next;
+        temp.next = null;
+
+        return temp;
+    }
+
+    public node deleteend(){
+        if (head==null || head.next==null) {
+            return head;
+        }
+
+        else{
+            node curr = head;
+            node pre = null;
+
+            while (curr.next != null) {
+                pre = curr;
+                curr = curr.next;
+            }
+            pre.next = null;
+            return curr;
+        }
+    }
+
+    public void delbypos(int pos){
+        if (pos==1) {
+            head = head.next;
+        }
+
+        else{
+            node curr =  head;
+            int count = 1;
+
+            while (pos-1>count) {
+                curr = curr.next;
+                count++;
+            }
+
+            curr.next = curr.next.next;
+        }
+    }
+
+    public void delkey(int val){
+        node curr = head;
+        
+        while (curr!=null && curr.next!=null) {
+            if (curr.next.data == val) {
+                curr.next = curr.next.next;
+                
+            }
+            curr = curr.next;
+        }
+    }
+}
