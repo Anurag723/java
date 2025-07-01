ce7a2a632b7bc749d857a7b51bcb2a2c172c730e
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-07-01 18:07:11 +0000

    queue implementation

---
+++ code/queue/implementation.java
@@ -0,0 +1,30 @@
+package code.queue;
+
+import java.util.*;
+
+public class implementation {
+    private node front;
+    private node rear;
+    private int length;
+
+    public static class node{
+        private int data;
+        private node next;
+
+        public node(int val){
+            this.data = val;
+            this.next = null;
+        }
+    }
+
+    public int length(){
+        return length;
+    }
+
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    public static void main(String[] args) {
+    }
+}
