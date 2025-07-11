5065d9b99a9d1d67f5a5d218f1d47458294a0a1e
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-07-11 18:43:15 +0000

    Binary Search Tree implementation

---
+++ code/binary tree/bindarysearchtree.java
@@ -0,0 +1,20 @@
+// binary tree is same as binary search tree bst is only different because its children are arranged as smaller on left and larger on right side of root...
+
+
+class binarysearchtree {
+    private node root;
+
+    private static class node {
+        private node left;
+        private int data;
+        private node right;
+
+        public node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
