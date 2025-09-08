9a57a0d0e02552c4703ac674784e95a1e3640faa
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-09-08 18:47:46 +0000

    reverse words

---
+++ code/array/string/ReverseWords.java
@@ -0,0 +1,31 @@
+package string;
+
+import javax.crypto.AEADBadTagException;
+
+class ReverseWords{
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=arr.length-1; i>=0; i--){
+            sb.append(arr[i]);
+
+            if(i!=0){
+                sb.append(" ");
+            }
+        }
+
+        
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseWords().reverseWords("  hello world  "));       // "world hello"
+        System.out.println(new ReverseWords().reverseWords("a good   example"));      // "example good a"
+        System.out.println(new ReverseWords().reverseWords("    "));                  // ""
+        System.out.println(new ReverseWords().reverseWords("one"));                   // "one"
+        System.out.println(new ReverseWords().reverseWords("  multiple   spaces "));  // "spaces multiple"
+
+    }
+}
