e16cae4bcfb8788349a2661981156bb6997e9451
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-11-11 19:21:11 +0000

    optimised

--- code/array/PlusOne.java
+++ code/array/PlusOne.java
@@ -24,6 +24,24 @@ public class PlusOne {
         return sol;
     }
 
+    public static int[] plusOneII(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+            digits[i]=0;
+            }
+        }
+         digits=new int[digits.length+1];
+         digits[0]=1;
+         return digits;
+    }
+
     public static void main(String[] args) {
         int[] digits = {9, 9, 9, 9};
 
