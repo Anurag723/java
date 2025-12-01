6ceedad31b301d37cb1c794c1b00b6c3d495e42f
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-12-01 17:43:56 +0000

    Regular Expression

---
+++ code/array/RegEx.java
@@ -0,0 +1,50 @@
+public class RegEx {
+
+    // LeetCode Regular Expression Matching
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // Handle patterns like a*, a*b*, a*b*c*
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+
+                if (pc == sc || pc == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // Zero occurrences
+                    dp[i][j] = dp[i][j - 2];
+
+                    // One or more occurrences if preceding char matches
+                    char prev = p.charAt(j - 2);
+                    if (prev == sc || prev == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        RegEx regex = new RegEx();
+
+        System.out.println(regex.isMatch("aa", "a"));        // false
+        System.out.println(regex.isMatch("aa", "a*"));       // true
+        System.out.println(regex.isMatch("ab", ".*"));       // true
+        System.out.println(regex.isMatch("aab", "c*a*b"));   // true
+        System.out.println(regex.isMatch("mississippi", "mis*is*p*.")); // false
+    }
+}
