c648cc56733813290fa182ba904ae3ec353ba1cf
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-12-06 18:11:58 +0000

    Add two Binary numbers

---
+++ code/study/AddBinary.java
@@ -0,0 +1,30 @@
+public class AddBinary {
+
+    public static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry == 1) {
+            int sum = carry;
+
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);   // current bit
+            carry = sum / 2;      // carry for next step
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        String a = "1010";
+        String b = "1011";
+
+        String result = addBinary(a, b);
+        System.out.println("Result: " + result);
+    }
+}
