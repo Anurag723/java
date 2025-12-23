2e40ae28d54a91783b5ab83345f39caa91e1f411
Anurag723 <64183818+Anurag723@users.noreply.github.com>
2025-12-23 17:29:57 +0000

    Reverse ll by index

--- code/study/DecodeWays.java
+++ code/study/DecodeWays.java
@@ -40,4 +40,29 @@ public class DecodeWays {
 
         return count;
     }
+
+    public int numDecodingsOptimise(String s) {
+        int n = s.length();
+        if (n == 0 || s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            // One digit
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+
+            // Two digits
+            int two = (s.charAt(i - 2) - '0') * 10 + (s.charAt(i - 1) - '0');
+            if (two >= 10 && two <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+
 }
